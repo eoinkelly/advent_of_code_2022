ec6cbb397df880b7251b04069f91078df32baf36
Eoin Kelly <eoin@eoinkelly.info>
2022-12-06 19:22:41 +1300

    Day 6

---
+++ day_6.rb
@@ -0,0 +1,19 @@
+def find_end_of_first_window(char_stream, window_size:)
+  (0..(char_stream.length - window_size)).each do |i|
+    window = char_stream[i, window_size]
+    return i + window_size if window.length == window.chars.uniq.join.length
+  end
+end
+
+# part_1_ex_1 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+# part_1_ex_2 = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+# puts "Part 2: #{find_end_of_first_window(part_1_ex_1, window_size: 4)}"
+# puts "Part 2: #{find_end_of_first_window(part_1_ex_2, window_size: 4)}"
+
+puts "Part 1: #{find_end_of_first_window(File.read("day_6_input.txt", chomp: true), window_size: 4)}"
+
+# part_2_ex_1 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+# part_2_ex_2 = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+# puts "Part 2: #{find_end_of_first_window(part_2_ex_1, window_size: 14)}"
+# puts "Part 2: #{find_end_of_first_window(part_2_ex_2, window_size: 14)}"
+puts "Part 2: #{find_end_of_first_window(File.read("day_6_input.txt", chomp: true), window_size: 14)}"
